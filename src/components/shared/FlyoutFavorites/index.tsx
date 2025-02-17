3fe264ded3f754a9c8f36a6a43ec6a2ec94b92f9
unknown <nomad8t@gmail.com>
2025-02-18 00:08:23 +0300

    feat: add FlyoutFavorites

--- src/components/shared/FlyoutFavorites/index.tsx
+++ src/components/shared/FlyoutFavorites/index.tsx
@@ -13,27 +13,24 @@ export interface FlyoutFavoritesProps {
 export function FlyoutFavorites({ className }: Readonly<FlyoutFavoritesProps>) {
   const { coinsIds } = useAppSelector((state) => state.favorite);
   const dispatch = useAppDispatch();
-  const isEmpty = coinsIds.length === 0;
+
+  const isFavoritesEmpty = coinsIds.length === 0;
 
   const handleDownload = () => {
     console.log('download');
   };
 
   return (
-    <>
-      {!isEmpty && (
-        <div className={cn(classes.root, className)}>
-          <Flyout>
-            <div className={classes.selected}>Selected coins: {coinsIds.length}</div>
-            <div className={classes.buttons}>
-              <Button onClick={() => dispatch(removeAllFavorites())}>Unselect all</Button>
-              <Button variant="primary" onClick={handleDownload}>
-                Download
-              </Button>
-            </div>
-          </Flyout>
+    <div className={cn(classes.root, className)}>
+      <Flyout isOpen={!isFavoritesEmpty}>
+        <div className={classes.selected}>Selected coins: {coinsIds.length}</div>
+        <div className={classes.buttons}>
+          <Button onClick={() => dispatch(removeAllFavorites())}>Unselect all</Button>
+          <Button variant="primary" onClick={handleDownload}>
+            Download
+          </Button>
         </div>
-      )}
-    </>
+      </Flyout>
+    </div>
   );
 }
--- src/components/ui/Flyout/index.tsx
+++ src/components/ui/Flyout/index.tsx
@@ -1,12 +1,18 @@
 import cn from 'classnames';
+import React from 'react';
 
 import classes from './flyout.module.scss';
 
 export interface FlyoutProps {
   className?: string;
   children: React.ReactNode;
+  isOpen: boolean;
 }
 
-export function Flyout({ className, children }: Readonly<FlyoutProps>) {
-  return <div className={cn(classes.root, className)}>{children}</div>;
+export function Flyout({ className, children, isOpen }: Readonly<FlyoutProps>) {
+  return (
+    <div className={cn(classes.root, className, { [classes['is-open']]: isOpen }, { [classes['is-closed']]: !isOpen })}>
+      {children}
+    </div>
+  );
 }
