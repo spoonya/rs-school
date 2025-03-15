500111e340d555fba1742722c91b446f5e3f89ad
unknown <nomad8t@gmail.com>
2025-03-15 19:06:25 +0300

    feat: add show/hide password toggle

--- src/components/ui/TextField/index.tsx
+++ src/components/ui/TextField/index.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames';
-import { DetailedHTMLProps, forwardRef, InputHTMLAttributes } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
+import { DetailedHTMLProps, forwardRef, InputHTMLAttributes, useState } from 'react';
 
 import { FormControlError, Input } from '@/components/ui';
 
@@ -14,17 +15,40 @@ interface TextFieldProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInput
 }
 
 export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
-  ({ className, label, placeholder, error, errorText, ...props }, ref) => {
+  ({ className, label, placeholder, error, errorText, type, ...props }, ref) => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const isPasswordField = type === 'password';
+
     return (
       <div className={cn(classes.root, className)}>
         <label className={classes.wrapper}>
           {label && <span className={classes.label}>{label}</span>}
-          <Input
-            ref={ref}
-            className={cn(classes.input, { [classes.errorInput]: error })}
-            placeholder={placeholder}
-            {...props}
-          />
+          <div className={classes.inputContainer}>
+            <Input
+              ref={ref}
+              className={cn(classes.input, {
+                [classes.errorInput]: error,
+                [classes.inputWithToggle]: isPasswordField,
+              })}
+              placeholder={placeholder}
+              type={isPasswordField ? (isPasswordVisible ? 'text' : 'password') : type}
+              {...props}
+            />
+            {isPasswordField && (
+              <span
+                role="button"
+                onClick={() => setIsPasswordVisible((prev) => !prev)}
+                className={cn(classes.toggleButton)}
+                aria-label={isPasswordVisible ? 'Show password' : 'Hide password'}
+              >
+                {isPasswordVisible ? (
+                  <EyeOff size={20} className={classes.icon} />
+                ) : (
+                  <Eye size={20} className={classes.icon} />
+                )}
+              </span>
+            )}
+          </div>
           {error && <FormControlError className={classes.error}>{errorText}</FormControlError>}
         </label>
       </div>
